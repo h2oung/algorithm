38ac81e4a1fb38b4622a59697933e450e1f4df3b
h2oung <139978853+h2oung@users.noreply.github.com>
2025-07-24 18:27:28 +0900

    [Silver IV] Title: 스택, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.cc
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N;
+    cin >> N;
+    stack<int> s;
+
+    while (N--) {
+        string cmd;
+        cin >> cmd; // 원하는 명령어 입력받음 
+
+        if (cmd == "push") {
+            int x;
+            cin >> x;
+            s.push(x);
+        }
+        else if (cmd == "pop") {
+            if (s.empty()) cout << -1 << '\n';
+            else {
+                cout << s.top() << '\n';
+                s.pop();
+            }
+        }
+        else if (cmd == "size") {
+            cout << s.size() << '\n';
+        }
+        else if (cmd == "empty") {
+            cout << (s.empty() ? 1 : 0) << '\n';
+        }
+        else if (cmd == "top") {
+            if (s.empty()) cout << -1 << '\n';
+            else cout << s.top() << '\n';
+        }
+    }
+}
